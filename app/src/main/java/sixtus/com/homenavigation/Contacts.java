d21ac1196a2301c9d0f6ee7b6060131f94ce69e9
DrCod <polleronny@yahoo.com>
2017-03-23 03:49:24 +0000

    Home Activity commits

---
+++ app/src/main/java/sixtus/com/homenavigation/Contacts.java
@@ -0,0 +1,48 @@
+
+
+public class Contacts {
+	
+	
+	String FirstName,LastName,Occupation,EmailAddress,Password;
+	  int MobileNumber;
+	  
+  public String getFirstName() {
+		return FirstName;
+	}
+	public void setFirstName(String firstName) {
+		this.FirstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {	
+		this.LastName = lastName;
+	}
+	public String getOccupation() {
+		return Occupation;
+	}
+	public void setOccupation(String occupation) {
+		this.Occupation = occupation;
+	}
+	public String getEmailAddress() {
+		return EmailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.EmailAddress = emailAddress;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		this.Password = password;
+	}
+	public int getMobileNumber() {
+		return MobileNumber;
+	}
+	public void setMobileNumber(int mobileNumber) {
+		this.MobileNumber = mobileNumber;
+	}
+}
+
+  
+
